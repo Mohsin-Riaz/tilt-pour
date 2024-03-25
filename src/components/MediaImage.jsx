3dbefa6f4507d037911b844d6ea4ba30d69f5b81
Mohsin Riaz <riaz.mohsin@gmail.com>
2024-03-25 11:20:27 -0400

    init commit

---
+++ src/components/MediaImage.jsx
@@ -0,0 +1,35 @@
+import { Magnifier, MagnifierContainer } from '@datobs/react-image-magnifiers';
+import React, { useState } from 'react';
+import { FaMagnifyingGlassPlus } from 'react-icons/fa6';
+
+const MediaImage = (prop) => {
+    const { image } = prop;
+    const [hover, setHover] = useState(false);
+    const [show, setShow] = useState(true);
+    return (
+        <div
+            className="media-wrapper"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            style={{ width: '400px' }}
+        >
+            <FaMagnifyingGlassPlus
+                className={'magnify-icon'}
+                style={{
+                    opacity: hover ? 0.6 : null,
+                    display: show ? null : 'none',
+                }}
+            />
+            <MagnifierContainer>
+                <Magnifier
+                    imageSrc={image}
+                    dragToMove={false}
+                    onZoomStart={() => setShow(false)}
+                    onZoomEnd={() => setShow(true)}
+                />
+            </MagnifierContainer>
+        </div>
+    );
+};
+
+export default MediaImage;
